1bb4dc557cd3d82fdfa5b85acbe3b94ba5f84fee
MorenoLopez <moreno.rarivo@gmail.com>
2026-09-08 16:43:42 +0300

    Zone

--- src/models/zone.py
+++ src/models/zone.py
@@ -7,7 +7,7 @@
 #   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/09/08 16:28:02 by horarivo            #+#    #+#            #
-#   Updated: 2026/09/08 16:39:39 by horarivo           ###   ########.fr      #
+#   Updated: 2026/09/08 16:43:16 by horarivo           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -21,13 +21,13 @@ class Zone:
                 x: int,
                 y: int,
                 zone_type: str = "normal",
-                max_drones: int,
+                max_drones: int = 1,
                 color: Optional[str] = None):
-        self.name: str = name
-        self.x: int = x
-        self.y: int = y
-        self.zone_type: str = zone_type
-        self.max_drones: int = max_drones
-        self.color: Optional[str] = color
+        self.name = name
+        self.x = x
+        self.y = y
+        self.zone_type = zone_type
+        self.max_drones = max_drones
+        self.color = color
         
         
