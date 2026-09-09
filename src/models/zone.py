9cdf3b4e30c5a572f31b528b1e1ed55e9fe1eed5
MorenoLopez <moreno.rarivo@gmail.com>
2026-09-09 15:47:33 +0300

    is_start, is_end

--- src/models/zone.py
+++ src/models/zone.py
@@ -7,7 +7,7 @@
 #   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/09/08 16:28:02 by horarivo            #+#    #+#            #
-#   Updated: 2026/09/09 11:51:10 by horarivo           ###   ########.fr      #
+#   Updated: 2026/09/09 15:38:16 by horarivo           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -30,5 +30,6 @@ class Zone:
         self.zone_type = zone_type
         self.max_drones = max_drones
         self.color = color
-        
+        self.is_start: bool = False
+        self.is_end: bool = False
         
