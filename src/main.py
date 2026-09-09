9d771d16edc2b87401e8eec22acd837016f9d8d3
MorenoLopez <moreno.rarivo@gmail.com>
2026-09-09 14:03:14 +0300

    parsing error OK

--- src/main.py
+++ src/main.py
@@ -7,16 +7,20 @@
 #   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/09/08 16:15:20 by horarivo            #+#    #+#            #
-#   Updated: 2026/09/09 10:53:17 by horarivo           ###   ########.fr      #
+#   Updated: 2026/09/09 11:59:57 by horarivo           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 
 import sys
-from parser import Parser
+from parser import Parser, ParseError
 
 def main():
-    parsedmap = Parser().parse(sys.argv[2])
+    try:
+        parsed = Parser().parse(sys.argv[2])
+    except (ParseError, OSError) as exc:
+        print(f"Error: {exc}")
+        sys.exit(1)
 
 if __name__=="__main__":
     main()
